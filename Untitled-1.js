513f065702f41a3e45a37742bd3e626db8f47328
Maxim <example@gmail.com>
2022-04-08 22:21:57 +0300

    practice1

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,2 +1,16 @@
-a = 1;
-console.log(a);
+const NumberOfFilms = +prompt("Скільки фільмів ви вже подивились?");
+const personalMovieDB = {
+    count: NumberOfFilms,
+    movies: {},
+    actors: {},
+    gender: [],
+    privat: false
+};
+const a = prompt("ОДин із останніх переглянутих фільмів?", ""),
+    b = prompt("на скільки оціните його?");
+c = prompt("ОДин із останніх переглянутих фільмів?", ""),
+    d = prompt("на скільки оціните його?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
