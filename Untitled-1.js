c54416a0ce22c66457eb9edf8f2d868c1290d975
Maxim <example@gmail.com>
2022-04-08 23:21:20 +0300

    practice2

--- Untitled-1.js
+++ Untitled-1.js
@@ -6,11 +6,29 @@ const personalMovieDB = {
     gender: [],
     privat: false
 };
-const a = prompt("ОДин із останніх переглянутих фільмів?", ""),
-    b = prompt("на скільки оціните його?");
-c = prompt("ОДин із останніх переглянутих фільмів?", ""),
-    d = prompt("на скільки оціните його?");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("ОДин із останніх переглянутих фільмів?", ""),
+        b = prompt("на скільки оціните його?", "");
+
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+if (personalMovieDB.count < 10) {
+    console.log("Переглянуто доволі мало фільмів");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("ВИ класичний глядач");
+}
+if (personalMovieDB.countЮ30) {
+    console.log("Ви кіноман");
+} else {
+    console.log('error')
+}
+
 console.log(personalMovieDB);
